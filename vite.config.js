8b626d2d85b125adcd43c7bae4a753b8cfe7074c
Zhangshuai <15446729+zhangshuai2790@user.noreply.gitee.com>
2026-05-11 13:35:13 +0800

    1.0

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  base: '/green-econnomic/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
